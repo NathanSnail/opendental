58127afaadb5ae23283af22abc0e56a746bae615
Nahtan <nathan.titirangi@gmail.com>
2024-01-06 15:49:49 +1300

    seems to be receiving good

--- OpenDental/Main Modules/AsyncCommlogs.cs
+++ OpenDental/Main Modules/AsyncCommlogs.cs
@@ -40,23 +40,34 @@ namespace OpenDental.Main_Modules
                 {*/
                 await Task.Delay(10 * 1000); // this is at start because there are race conditions i don't want to fix. We need to wait for init to be done before we can use comlogs
                 Console.WriteLine("!!");
-                Commlog log = new Commlog();
-                log.Note = "hello!";
+                
                 var response = await sharedClient.GetAsync(send);
                 var text = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(text);
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.LoadXml(text);
                 var list = xmlDoc.ChildNodes[1];
-                Console.WriteLine(list.InnerText);
                 foreach (XmlElement child in list.ChildNodes)
                 {
-                    Console.WriteLine(child.InnerText);
-                    string from = child.GetAttribute("From");
+                    string from = child.ChildNodes[0].InnerText;
                     Console.WriteLine(from);
+                    string msgText = child.ChildNodes[2].InnerText;
+                    Console.WriteLine(msgText);
+                    string msgTime = child.ChildNodes[11].InnerText;
+                    var time = DateTime.Parse(msgTime);
+                    Console.WriteLine(msgText);
+                    var patients = Patients.GetPatientsByPhone(from.Substring(3), "+64");
+                    Console.WriteLine(patients.Count);
+                    Commlog log = new Commlog();
+                    if (patients.Count != 0)
+                    {
+                        log.PatNum = patients[0].PatNum;
+                    }
+                    log.Note = "Text message received: " + msgText;
+                    log.Mode_ = CommItemMode.Text;
+                    log.CommDateTime = time;
+                    Commlogs.Insert(log);
                 }
-                log.PatNum = 22;
-                Commlogs.Insert(log);
+               
 /*                }
                 catch
                 {
